0a7e63dfcb8d92a66cf82a5f235410354da08a8b
osharygi <olga.sphere@gmail.com>
2016-01-06 10:36:27 -0500

    le page Profil est ajouté

---
+++ profil.php
@@ -0,0 +1,23 @@
+<?php
+require_once  '_defines.php';
+require_once 'data/_main_data.php';
+$site_data[PAGE_ID] = 'Profil';
+require_once 'view_parts/_header.php';
+?>
+<div id="main">
+    <div class="container">
+<h1>Votre profil</h1>
+
+     </br>
+    <form name="upload" action="download_img.php" method="POST" ENCTYPE="multipart/form-data">
+        Choisisez votre photo:
+        <p><input type="file" name="userfile"></p>
+        <p></p><img data-src="holder.js/200x200" class="img-thumbnail" alt="200x200"
+                    src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgdmlld0JveD0iMCAwIDIwMCAyMDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzIwMHgyMDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNTIxNzhjOTdiOSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1MjE3OGM5N2I5Ij48cmVjdCB3aWR0aD0iMjAwIiBoZWlnaHQ9IjIwMCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9Ijc1LjUiIHk9IjEwNC41Ij4yMDB4MjAwPC90ZXh0PjwvZz48L2c+PC9zdmc+"
+                    data-holder-rendered="true" style="width: 200px; height: 200px;"></p>
+
+    </form></div>
+
+    <?php
+    ?>
+    <?php require_once 'view_parts/_footer.php'; ?></div>

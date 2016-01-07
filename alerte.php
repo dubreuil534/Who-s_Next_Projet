9a112f4d5f774df3f7a4f2384380c92b690d94ce
Dominic Dubreuil <dubreuil534@hotmail.com>
2016-01-07 13:59:01 -0500

    Deboguage page alerte.php

--- alerte.php
+++ alerte.php
@@ -1,8 +1,8 @@
 <?php
-require_once 'view_parts/_header.php';
 require_once '_defines.php';
 require_once 'data/_main_data.php';
 $site_data[PAGE_ID] = 'Alerte';
+require_once 'view_parts/_header.php';
 ?>
 <div class="alert alert-warning" role="alert">
     <strong>Oufff!</strong> Veuillez vous connectez pour accéder à la page.

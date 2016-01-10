301d2e2c582d4f59680c92afae0a633cce5d98da
Dominic Dubreuil <dubreuil534@hotmail.com>
2016-01-10 17:56:12 -0500

    Changement code de verifcation du hashing du fichier connecter.php

--- Connecter.php
+++ Connecter.php
@@ -1,26 +1,33 @@
 <?php
 include("Server_Conf.php");
-
+include("db/_common.php");
 
 session_start();
 		try{
 			$cn=new PDO("mysql:host=".HOST_NAME.";dbname=".DATABASE_NAME,USER_NAME,PASSEWORD);
 			$cn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$qt="SELECT username,email,age,password,sex,orientation,region,buts,description,statut FROM user WHERE email=? AND password=?";
+		$qt="SELECT username,email,age,password,sex,orientation,region,buts,description,statut FROM user WHERE email=?";
 		$stmt=$cn->prepare($qt);
 		$stmt->bindValue(1, $_POST['email']);		
-		$stmt->bindValue(2, $_POST['pass']);
+		//$stmt->bindValue(2, passwd_encrypt($_POST['pass']));
 		
 				if($stmt->execute()){
 						$ligne=$stmt->fetch(PDO::FETCH_OBJ);
 				   $_SESSION['user']=$ligne;
 			
-			echo $ligne->username;
-		$qt="UPDATE user SET statut=1 WHERE email='".$_POST['email']."';";
-			$stm=$cn->prepare($qt);
-			$stm->execute();
-						if (!empty($_SESSION['user']))
-							header('Location:index.php');
+			
+						if (!empty($_SESSION['user'])){
+							        
+									if(passwd_check($_POST['pass'],$ligne->password)){
+							
+										$qt="UPDATE user SET statut=1 WHERE email='".$_POST['email']."';";
+										$stm=$cn->prepare($qt);
+										$stm->execute();
+								
+										header('Location:index.php');
+									}else
+									header('Location:alerte.php');
+						}
 						else
 							header('Location:alerte.php');
 
